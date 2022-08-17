082062e13834b96d929f0044e2e6f916040306b1
SiddiquiMonis401 <monisms15@gmail.com>
2022-08-17 19:35:27 +0500

    add job dsl

--- job_dsl/nodejs.groovy
+++ job_dsl/nodejs.groovy
@@ -1,8 +1,29 @@
+#!/usr/bin/env groovy
+import hudson.tools.InstallSourceProperty
+import jenkins.model.Jenkins
+import jenkins.plugins.nodejs.tools.NodeJSInstallation
+import jenkins.plugins.nodejs.tools.NodeJSInstaller
+import static jenkins.plugins.nodejs.tools.NodeJSInstaller.DEFAULT_NPM_PACKAGES_REFRESH_HOURS
+
+final versions = [
+        'NodeJS 8.x': '8.16.1'
+]
+
+Jenkins.instance.getDescriptor(NodeJSInstallation).with {
+    installations = versions.collect {
+        new NodeJSInstallation(it.key, null, [
+                new InstallSourceProperty([
+                        new NodeJSInstaller(it.value, null, DEFAULT_NPM_PACKAGES_REFRESH_HOURS)
+                ])
+        ])
+    }  as NodeJSInstallation[]
+}
+
 job('NodeJS example') {
     scm {
         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-      node / gitConfigName('DSL User')
-      node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
