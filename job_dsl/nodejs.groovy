5d61dcbe4cace783ce553d137c02845ea6b3465f
SiddiquiMonis401 <monisms15@gmail.com>
2022-08-17 19:18:21 +0500

    add job dsl

--- job_dsl/nodejs.groovy
+++ job_dsl/nodejs.groovy
@@ -1,18 +1,18 @@
 job('NodeJS example') {
     scm {
         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+      node / gitConfigName('DSL User')
+      node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('NodeJS') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+        nodejs('recent_node') // this is the name of the NodeJS installation in
+    // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        shell("npm install")
+        shell('npm install')
     }
 }
